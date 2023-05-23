b8b19a3b5a22b88e5e86f64b695eb2d73fa845fb
Ines Alvergne <i.alvergne@gmail.com>
2023-05-23 19:10:58 -0400

    a user can list movies and add a bookmark between a movie and a list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, expect: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
+  # get '/lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get '/lists/:id', to: 'lists#show', as: :list
+  # post 'lists', to: 'lists#create'
+
+  # get 'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
 end
